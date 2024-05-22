c45930d019939a2b87fa6443869bfe2189d600ca
Naren-2001-k <k.naren2001@gmail.com>
2024-05-22 12:16:58 +0530

    May22-2024

--- app.js
+++ app.js
@@ -1,6 +1,80 @@
+// Server Creation
 const http=require("http");
 http.createServer(function(req,res){
     res.write("Server is successfully created")
     res.end()
 
 }).listen(3000)
+
+// OS Module
+const os=require('os')
+console.log(os.hostname(),"Hostname")
+console.log(os.type(),"Type")
+
+// Make Dir
+const fs=require("fs")
+fs.mkdir('demo',(err)=>{
+    if(err){
+        console.log("Error",err)
+    }else{
+        console.log("Create Successfully")
+    }
+})
+
+// Remove Dir
+fs.rmdir('demo1',(err)=>{
+    if(err){
+        console.log("Error",err)
+    }else{
+        console.log("Remove file Successfully")
+    }
+})
+
+// Write File
+const folderPath = './demo';
+const fileName = 'example.txt'; 
+const newfileName = 'fileSystem.txt'; 
+const content='Hi Narendhiran'
+fs.writeFile(`${folderPath}/${fileName}`,content,(err)=>{
+    if(err){
+        console.log("File is not created")
+    }else{
+        console.log("File is created successfully")
+    }
+})
+
+// Append File
+fs.appendFile(`${folderPath}/${fileName}`,"Welcome",(err)=>{
+    if(err){
+        console.log("File is not update successfully")
+    }else{
+        console.log("File is update successfully")
+    }
+})
+
+// Readfile
+fs.readFile(`${folderPath}/${fileName}`,(err,data)=>{
+    if(err){
+        console.log("File is not created successfully",err)
+    }else{
+        console.log(data.toString(),"Data Read Successfully")
+    }
+})
+
+// Unlink (delete file)
+fs.unlink("myFile.txt",(err)=>{
+    if(err){
+        console.log("File is already deleted")
+    }else{
+        console.log("File is deleted successfully")
+    }
+})
+
+// Rename File
+fs.rename(`${folderPath}/${fileName}`, `${folderPath}/${newfileName}`,(err)=>{
+    if(err){
+        console.log("File is not read",err)
+    }else{
+        console.log("File rename Successfully")
+    }
+})
